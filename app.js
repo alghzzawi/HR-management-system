ce96323169962b3b415dc9320fafce03f6d48897
alghzzawi <mohammadalghazzawi9@gmail.com>
2022-08-10 17:05:01 +0300

    dom v2

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 var id = 1000;
 
 var employeeList = [];
-function Employee (emplyeeName, employeeDepartment, employeelevel, employeeImage){
+function Employee (emplyeeName, employeeDepartment, employeelevel, employeeimage){
     this.employeeid = function() {
         return id++;
     };
     this.fullName = emplyeeName;
     this.department = employeeDepartment;
     this.level = employeelevel;
-    this.imageURL = employeeImage;
+    this.imageURL = employeeimage;
     this.salary = function () {
         let salary;
 
@@ -40,13 +40,13 @@ Employee.prototype.printInfo = function(){
     
 }
 
-let emp1 = new Employee("Ghazi Samer", "Administration","Senior","https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-vector%2Fgroup-people-illustration-set_52683-33806.jpg%3Fw%3D2000&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Fvectors%2Fpeople-illustration&tbnid=TyVr2KLEbF_rZM&vet=12ahUKEwirhOjlkrf5AhVohc4BHaWqBh8QMygAegUIARCkAQ..i&docid=VuMP-z8couTF4M&w=2000&h=1333&q=people&ved=2ahUKEwirhOjlkrf5AhVohc4BHaWqBh8QMygAegUIARCkAQ")
-const emp2 = new Employee("Lana Ali", "Finance", "Senior",'./empimag/lana.jpg')
-const emp3 = new Employee("Tamara Ayoub", "Marketing", "Senior",'./empimag/tamara.jpg')
-const emp4 = new Employee("Safi Walid", "Administration", "Mid-Senior",'./empEmage/safi.jpg')
-const emp5 = new Employee("Omar Zaid", "Development", "Senior",'./empEmage/omar.jpg')
-const emp6 = new Employee("Rana Saleh", "Development", "Junior",'./empEmage/rana.jpg')
-const emp7 = new Employee("Hadi Ahmad", "Finance", "Mid-Senior",'./empEmage/hadi.jpg')
+let emp1 = new Employee("Ghazi Samer", "Administration","Senior","./assets/ghazi.png")
+const emp2 = new Employee("Lana Ali", "Finance", "Senior",'./assets/lana.jpg')
+const emp3 = new Employee("Tamara Ayoub", "Marketing", "Senior",'./assets/tamara.jpg')
+const emp4 = new Employee("Safi Walid", "Administration", "Mid-Senior",'./assets/safi.jpg')
+const emp5 = new Employee("Omar Zaid", "Development", "Senior",'./assets/omar.png')
+const emp6 = new Employee("Rana Saleh", "Development", "Junior",'./assets/rana.jpg')
+const emp7 = new Employee("Hadi Ahmad", "Finance", "Mid-Senior",'./assets/hadi.jpg')
 
 // emp1.printInfo()
 // emp2.printInfo()
@@ -62,8 +62,9 @@ Employee.prototype.printData = function(){
 
     const div = document.createElement('div')
     let imgEmp = document.createElement('img')
-    imgEmp.src = this.employeeImage;
+    imgEmp.src = this.imageURL;
     imgEmp.alt = this.fullName;
+    
     div.appendChild(imgEmp)
 
 
